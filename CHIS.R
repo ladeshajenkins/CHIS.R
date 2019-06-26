932fa4b2a7386c1e39df9d71ac09dbfb3744d11c
ladeshajenkins <52169827+ladeshajenkins@users.noreply.github.com>
2019-06-26 11:55:07 -0400

    Update CHIS.R

--- CHIS.R
+++ CHIS.R
@@ -13,3 +13,20 @@ ggplot(adult, aes (x = BMI_P)) +
 # Age colored by BMI, binwidth = 1
 ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
   geom_histogram(binwidth = 1)
+# Keep adults younger than or equal to 84
+adult <- adult[adult$SRAGE_P <= 84, ]
+
+# Keep adults with BMI at least 16 and less than 52
+adult <- adult[adult$BMI_P >= 16 & adult$BMI_P < 52, ]
+
+# Relabel the race variable
+adult$RACEHPR2 <- factor(adult$RACEHPR2, labels = c("Latino",
+                                                    "Asian",
+                                                    "African American",
+                                                    "White"))
+
+# Relabel the BMI categories variable
+adult$RBMI <- factor(adult$RBMI, labels = c("Under-weight",
+                                            "Normal-weight",
+                                            "Over-weight",
+                                            "Obese"))
